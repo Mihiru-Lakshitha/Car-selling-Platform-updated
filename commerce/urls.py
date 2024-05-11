d4493b6211f79f634de8f9e18f116c9f0d0d1f24
Mihiru Lakshitha Silva <66376006+Mihiru-Lakshitha@users.noreply.github.com>
2024-05-11 13:15:23 +0530

    Add files via upload

---
+++ commerce/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import include, path
+from home.views import HomeView  # Import the home view
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", HomeView.as_view(), name='default_home'),  # Set the default URL to the home page
+    path("auctions/", include("auctions.urls")),
+    path("home/", include("home.urls")),
+    path('auctions/', include('car_prediction.urls')),
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
